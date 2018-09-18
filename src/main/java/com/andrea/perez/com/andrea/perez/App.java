9ef4aa10eb9ae7609f45914043d004b6d3f2e595
Andrea Perez <anmapemi06@hotmail.com>
2018-09-18 17:40:39 +0200

    first commit

---
+++ src/main/java/com/andrea/perez/com/andrea/perez/App.java
@@ -0,0 +1,13 @@
+package com.andrea.perez.com.andrea.perez;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
